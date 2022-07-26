14363e47854209a2fd4f7459aa413b1117d80a78
constantinandrei <andrei.todirascu@gmail.com>
2022-07-26 15:55:12 +0200

    install npm, nav added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('navbar')
+
+@include('partials.navbar')
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,25 @@
+@php
+    $navLinks = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+@endphp
+
+<nav>
+    <div class="py-3 bg-black"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col flex-grow-0 flex-shrink-1 d-flex align-items-center justify-content-between">
+                <div class="p-3">
+                    <img src="/img/dc-logo.png" alt=""/>
+                </div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-end">
+                <ul class="nav">
+                    @foreach ($navLinks as $link)
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{{ $link }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
