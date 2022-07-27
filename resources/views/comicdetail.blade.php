0fd3ba7916c8e646c9b9cbb954c3757b076bd13b
constantinandrei <andrei.todirascu@gmail.com>
2022-07-27 17:12:06 +0200

    styles

--- resources/views/comicdetail.blade.php
+++ resources/views/comicdetail.blade.php
@@ -1,7 +1,47 @@
 @extends('layouts.app')
 
+@section('page_title', $comic['title'])
+
 @section('page_content')
-
-    @dump($comic)
-
+    <div id="comic-detail"  class="section">
+        <div class="container">
+            
+            <div class="py-5">
+                <div class="row">
+                    <div class="col col-8">
+                        <h4 class="text-uppercase">{{ $comic['title'] }}</h4>
+                        <div class="bg-success">
+                            <div class="row">
+                                <div class="col col-8">
+                                    <div class="p-3">
+                                        <div class="row justify-content-between">
+                                            <div class="col">
+                                                <div class="text-white-50">U.S Price: <span class="text-white bg-transparent">{{$comic['price']}}</span></div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="text-end text-white-50">AVAILABLE</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col col-4">
+                                    <div class="text-white bg-transparent p-3">Check Availability</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-4">
+                        <div class="text-end">
+                            <h6 class="text-uppercase">advertisment</h6>
+                            <img class="img-fluid" src="{{ asset('img/adv.jpg') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    
+@include('partials.graybanner')
 @endsection
---
+++ resources/views/partials/graybanner.blade.php
@@ -0,0 +1,46 @@
+@php
+    $imgList = [[
+                    'link'=> 'img/buy-comics-digital-comics.png',
+                    'text'=> 'DIGITAL COMICS'
+                ],
+                    [
+                    'link'=> 'img/buy-comics-merchandise.png',
+                    'text'=> 'DC MERCHANDISE'
+                    ],
+                    [
+                    'link'=> 'img/buy-comics-subscriptions.png',
+                    'text'=> 'SUBSCRIPTIONS'
+                    ],
+                    [
+                    'link'=> 'img/buy-comics-shop-locator.png',
+                    'text'=> 'COMIC SHOP LOCATOR'
+                    ],
+                    [
+                    'link'=> 'img/buy-dc-power-visa.svg',
+                    'text'=> 'DC POWER VISA'
+                    ]
+                ]
+@endphp
+
+<div id="bluebanner" class="graybanner">
+    <div class="section py-5">
+        <div class="container">
+            <div class="row row-cols-5 gx-1">
+                @foreach ($imgList as $image)
+                <div class="col gap-3 d-flex align-items-center justify-content-start">
+                    <div class="my-col-1">
+                        <img src="{{ asset($image['link']) }}" alt="">
+                        
+                    </div>
+                    <div class="my-col">
+                        <div class="div">
+                            {{ $image['text'] }}
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                
+            </div>
+        </div>
+    </div>
+</div>
